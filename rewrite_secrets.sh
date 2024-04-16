8b1f8d548be942238cd2b0272d603e954d0373e0
lxg20082008 <61457095+lxg20082008@users.noreply.github.com>
2024-04-16 22:41:13 +0300

    Update rewrite_secrets.sh

--- rewrite_secrets.sh
+++ rewrite_secrets.sh
@@ -7,22 +7,28 @@ echo "Temporary file created successfully: $temp_toml_file"
 # Copy the original wrangler.toml content to a temporary file
 cp wrangler.toml "$temp_toml_file"
 
-# 添加'[vars]'块的新值
+# #####添加'[vars]'块的新值
 echo "UUID = \"$SECRET_UUID\"" >> "$temp_toml_file"
 echo "PATH = \"$SECRET_PATH\"" >> "$temp_toml_file"
 echo "HOST = \"$SECRET_HOST\"" >> "$temp_toml_file"
 echo "TOKEN = \"$SECRET_TOKEN\"" >> "$temp_toml_file"
 # 添加多行字符串的值，不添加额外的双引号和空行
-echo 'ADDAPI =' >> "$temp_toml_file"
+echo 'ADDAPI = """' >> "$temp_toml_file"
 echo "$SECRET_ADDAPI" >> "$temp_toml_file"
+echo '"""' >> "$temp_toml_file"
 
-echo 'ADDCSV =' >> "$temp_toml_file"
+echo 'ADDCSV = """' >> "$temp_toml_file"
 echo "$SECRET_ADDCSV" >> "$temp_toml_file"
+echo '"""' >> "$temp_toml_file"
 
-echo 'ADD =' >> "$temp_toml_file"
+echo 'ADD = """' >> "$temp_toml_file"
 echo "$SECRET_ADD" >> "$temp_toml_file"
+echo '"""' >> "$temp_toml_file"
+# 把 = """ 替换为 = (等号后面跟着一个空格)，并且移除所有的 """
+sed -i 's/= \"\"\"/= /g' \"$temp_toml_file\"
+sed -i 's/\"\"\"//g' \"$temp_toml_file\"
 
-# Print out the contents of the modified TOML file for verification
+# ####Print out the contents of the modified TOML file for verification
 cat "$temp_toml_file"
 
 # Expose the path of the temporary file as an environment variable for other steps to consume
