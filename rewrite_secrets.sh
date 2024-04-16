c32cfad37d5978c20a3b459d20d275ad7b29459a
lxg20082008 <61457095+lxg20082008@users.noreply.github.com>
2024-04-16 21:20:07 +0300

    Update rewrite_secrets.sh

--- rewrite_secrets.sh
+++ rewrite_secrets.sh
@@ -13,17 +13,17 @@ echo "PATH = \"$SECRET_PATH\"" >> "$temp_toml_file"
 echo "HOST = \"$SECRET_HOST\"" >> "$temp_toml_file"
 echo "TOKEN = \"$SECRET_TOKEN\"" >> "$temp_toml_file"
 # 添加多行字符串的值，不添加额外的双引号和空行
-echo "ADDAPI = \"\"\"" >> "$temp_toml_file"
+echo 'ADDAPI = """' >> "$temp_toml_file"
 echo "$SECRET_ADDAPI" >> "$temp_toml_file"
-echo "\"\"\"" >> "$temp_toml_file"
+echo '"""' >> "$temp_toml_file"
 
-echo "ADDCSV = \"\"\"" >> "$temp_toml_file"
+echo 'ADDCSV = """' >> "$temp_toml_file"
 echo "$SECRET_ADDCSV" >> "$temp_toml_file"
-echo "\"\"\"" >> "$temp_toml_file"
+echo '"""' >> "$temp_toml_file"
 
-echo "ADD = \"\"\"" >> "$temp_toml_file"
+echo 'ADD = """' >> "$temp_toml_file"
 echo "$SECRET_ADD" >> "$temp_toml_file"
-echo "\"\"\"" >> "$temp_toml_file"
+echo '"""' >> "$temp_toml_file"
 
 # Print out the contents of the modified TOML file for verification
 cat "$temp_toml_file"
